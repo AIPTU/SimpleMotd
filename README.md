6b7deb7bc8c5ce02664e16cce859cd79fd6c190d
AIPTU <anangp808@gmail.com>
2023-07-20 19:12:45 +0700

    Updates

--- README.md
+++ README.md
@@ -7,47 +7,48 @@ A PocketMine-MP plugin to change server motd easily.
 
 # Features
 
-- Supports `&` as formatting codes.
-- `{PREFIX}` can be used to display prefix motd message.
-- `{SUFFIX}` can be used to display suffix motd message.
-- `{TIME}` can be used to display currently date/time.
-- `{ONLINE_PLAYERS}` can be used to display currently online players on the server.
-- `{MAX_PLAYERS}` can be used to display maximum online players on the server.
-- Lightweight and open source ❤️
+- `Dynamic Message of the Day (MOTD)`: The plugin allows server administrators to set up a dynamic MOTD that changes periodically, showing different messages to players when they join the server.
+- `MOTD Configuration`: The plugin provides a configuration file that allows administrators to customize various aspects of the MOTD, including enabling or disabling it, setting the interval for message changes, specifying the messages to display, and configuring prefix and suffix text.
+- `Placeholder Support`: The plugin supports placeholders in the MOTD messages, allowing administrators to show dynamic information to players. Available placeholders include the maximum number of players supported by the server, the number of online players, the configured prefix and suffix, and the current time.
+- `Color Formatting`: The MOTD messages can be colored using "§" or "&" color codes, providing visual appeal and customization options for the server's welcome messages.
+- `MOTD Update Interval`: Administrators can set the interval (in seconds) between each MOTD change. This allows them to control how frequently the MOTD messages cycle through the configured options.
+- `Config Version Management`: The plugin ensures that the configuration file is up-to-date. If an outdated config file is provided, it generates a new one and backs up the old config, preventing compatibility issues.
+- `Error Handling`: The plugin includes error handling mechanisms to notify administrators in case of any configuration issues or critical errors during the generation of a new config.
+- `Date/Time Customization`: Administrators can customize the date and time format displayed in the MOTD to match their desired format, using the standard PHP date format codes.
+- `MOTD Enable/Disable`: Administrators have the flexibility to enable or disable the MOTD feature altogether. When disabled, the plugin will not proceed with displaying the MOTD messages.
+- `MOTD Scheduler`: The plugin uses the PocketMine scheduler to periodically change the MOTD messages based on the configured interval, ensuring a seamless and automatic rotation of messages.
 
 # Default Config
 ```yaml
----
 # Do not change this (Only for internal use)!
 config-version: 1.1
 
-# Available tags for motd messages:
-# - {MAX_PLAYERS}: Show the maximum number of players supported by the server.
-# - {ONLINE_PLAYERS}: Show the number of all online players.
-# - {PREFIX}: Show prefix.
-# - {SUFFIX}: Show suffix.
-# - {TIME}: Show current time.
-# Use "§" or "&" to color the message.
-
-# Prefix
+# Prefix used in MOTD messages. Use "§" or "&" to color the message.
 prefix: "&b»"
-# Suffix
+# Suffix used in MOTD messages. Use "§" or "&" to color the message.
 suffix: "&b«"
 # Date\Time format (replaced in {TIME}).
 # For format codes read https://php.net/manual/en/datetime.formats.php
 datetime-format: "H:i:s"
 
+# Message of the Day (MOTD) Configuration
 motd:
-  # Enable the motd.
+  # Enable the MOTD.
   enabled: true
-  # Motd interval (in seconds).
+  # MOTD interval (in seconds).
   time: 15
-  # Messages used to display motd.
+  # Messages used to display the MOTD. You can use placeholders to show dynamic information.
+  # Available placeholders:
+  # - {MAX_PLAYERS}: Show the maximum number of players supported by the server.
+  # - {ONLINE_PLAYERS}: Show the number of all online players.
+  # - {PREFIX}: Show the configured prefix.
+  # - {SUFFIX}: Show the configured suffix.
+  # - {TIME}: Show current time based on the 'datetime-format' setting.
+  # Use "§" or "&" to color the message.
   messages:
-    - "&a[{TIME}] {PREFIX} Welcome to my server {SUFFIX}"
-    - "&b[{TIME}] Current online: &a{ONLINE_PLAYERS}/{MAX_PLAYERS}"
-    - "&b[{TIME}] {PREFIX} Hello World &a{SUFFIX}"
-...
+    - "&a[{TIME}] {PREFIX} Welcome to our awesome server {SUFFIX}" # Example: [10:30:45] » Welcome to our awesome server «
+    - "&b[{TIME}] Current online: &a{ONLINE_PLAYERS}/{MAX_PLAYERS}" # Example: [10:30:45] Current online: 50/100
+    - "&b[{TIME}] {PREFIX} Thanks for joining! Have fun! {SUFFIX}" # Example: [10:30:45] » Thanks for joining! Have fun! «
 
 ```
 
--- src/SimpleMotd.php
+++ src/SimpleMotd.php
@@ -1,27 +1,12 @@
 <?php
 
 /*
+ * Copyright (c) 2021-2023 AIPTU
  *
- * Copyright (c) 2021 AIPTU
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ * @see https://github.com/AIPTU/SimpleMotd
  */
 
 declare(strict_types=1);
@@ -31,75 +16,122 @@ namespace aiptu\simplemotd;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
 use function array_rand;
 use function count;
 use function date;
-use function rename;
+use function is_array;
+use function is_bool;
+use function is_int;
+use function is_string;
 use function str_replace;
 
-final class SimpleMotd extends PluginBase
-{
+class SimpleMotd extends PluginBase {
 	private const CONFIG_VERSION = 1.1;
 
-	private TypedConfig $typedConfig;
-
-	public function onEnable(): void
-	{
+	public function onEnable() : void {
 		$this->checkConfig();
-
 		$this->loadMotd();
 	}
 
-	public function replaceVars(string $str, array $vars): string
-	{
+	/**
+	 * Replaces variables in a given string with their corresponding values from the provided associative array.
+	 */
+	private function replaceVars(string $str, array $vars) : string {
 		foreach ($vars as $key => $value) {
 			$str = str_replace('{' . $key . '}', (string) $value, $str);
 		}
 		return $str;
 	}
 
-	public function getTypedConfig(): TypedConfig
-	{
-		return $this->typedConfig;
-	}
-
-	private function checkConfig(): void
-	{
+	/**
+	 * Checks and manages the configuration for the plugin.
+	 * Generates a new configuration if an outdated one is provided and backs up the old config.
+	 */
+	private function checkConfig() : void {
 		$this->saveDefaultConfig();
+		$config = $this->getConfig();
 
-		if (!$this->getConfig()->exists('config-version') || ($this->getConfig()->get('config-version', self::CONFIG_VERSION) !== self::CONFIG_VERSION)) {
-			$this->getLogger()->warning('An outdated config was provided attempting to generate a new one...');
-			if (!rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml')) {
-				$this->getLogger()->critical('An unknown error occurred while attempting to generate the new config');
+		if (!$config->exists('config-version') || $config->get('config-version', self::CONFIG_VERSION) !== self::CONFIG_VERSION) {
+			$this->getLogger()->warning('An outdated config was provided; attempting to generate a new one...');
+
+			$oldConfigPath = Path::join($this->getDataFolder(), 'config.old.yml');
+			$newConfigPath = Path::join($this->getDataFolder(), 'config.yml');
+
+			$filesystem = new Filesystem();
+			try {
+				$filesystem->rename($newConfigPath, $oldConfigPath);
+			} catch (IOException $e) {
+				$this->getLogger()->critical('An error occurred while attempting to generate the new config: ' . $e->getMessage());
 				$this->getServer()->getPluginManager()->disablePlugin($this);
+				return;
 			}
+
 			$this->reloadConfig();
 		}
-
-		$this->typedConfig = new TypedConfig($this->getConfig());
 	}
 
-	private function loadMotd(): void
-	{
-		$typedConfig = $this->getTypedConfig();
+	/**
+	 * Loads and schedules the MOTD (Message of the Day) to be displayed periodically.
+	 * If MOTD is not enabled in the configuration, it will skip the process.
+	 */
+	private function loadMotd() : void {
+		$config = $this->getConfig();
 
-		if ($typedConfig->getBool('motd.enabled', true)) {
-			$time = $typedConfig->getInt('motd.time', 15) * 20;
-
-			$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(
-				function () use ($typedConfig): void {
-					$messages = $typedConfig->getStringList('motd.messages');
-					$message = $messages[array_rand($messages)];
-
-					$this->getServer()->getNetwork()->setName(TextFormat::colorize($this->replaceVars($message, [
-						'MAX_PLAYERS' => $this->getServer()->getMaxPlayers(),
-						'ONLINE_PLAYERS' => count($this->getServer()->getOnlinePlayers()),
-						'PREFIX' => $typedConfig->getString('prefix'),
-						'SUFFIX' => $typedConfig->getString('suffix'),
-						'TIME' => date($typedConfig->getString('datetime-format', 'H:i:s')),
-					])));
-				}
-			), $time);
+		$motdEnabled = $config->getNested('motd.enabled', true);
+		if (!is_bool($motdEnabled)) {
+			$this->getLogger()->warning('Invalid value for "motd.enabled" in the config. Expected a boolean value.');
+			return;
 		}
+
+		if (!$motdEnabled) {
+			return;
+		}
+
+		$time = $config->getNested('motd.time', 15);
+		if (!is_int($time) || $time <= 0) {
+			$this->getLogger()->warning('Invalid value for "motd.time" in the config. Expected a positive integer value.');
+			return;
+		}
+
+		$time *= 20;
+
+		$messages = $config->getNested('motd.messages', []);
+		if (!is_array($messages) || count($messages) === 0) {
+			$this->getLogger()->warning('Invalid or empty "motd.messages" in the config. Expected a non-empty array of MOTD messages.');
+			return;
+		}
+
+		$datetimeFormat = $config->get('datetime-format', 'H:i:s');
+		if (!is_string($datetimeFormat)) {
+			$this->getLogger()->warning('Invalid value for "datetime-format" in the config. Expected a string value.');
+			return;
+		}
+
+		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function () use ($config, $messages, $datetimeFormat) : void {
+			$message = $messages[array_rand($messages)];
+
+			$prefix = $config->get('prefix', '');
+			$suffix = $config->get('suffix', '');
+
+			if (!is_string($prefix) || !is_string($suffix)) {
+				$this->getLogger()->warning('Invalid value for "prefix" or "suffix" in the config. Expected string values.');
+				return;
+			}
+
+			$vars = [
+				'MAX_PLAYERS' => $this->getServer()->getMaxPlayers(),
+				'ONLINE_PLAYERS' => count($this->getServer()->getOnlinePlayers()),
+				'PREFIX' => $prefix,
+				'SUFFIX' => $suffix,
+				'TIME' => date($datetimeFormat),
+			];
+
+			$formattedMessage = $this->replaceVars($message, $vars);
+
+			$this->getServer()->getNetwork()->setName(TextFormat::colorize($formattedMessage));
+		}), $time);
 	}
 }
